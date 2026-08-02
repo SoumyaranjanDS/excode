8ba6204e8994c6a71bd2d65043211db108901ed0
Soumyaranjan <soumyaranjansahoo97292@gmail.com>
2026-08-02 14:51:08 +0530

    fix navbar role issues

--- client/src/components/PillNav.jsx
+++ client/src/components/PillNav.jsx
@@ -228,7 +228,6 @@ const PillNav = ({
                 to={items[0]?.href || '/'}
                 aria-label="Home"
                 onMouseEnter={handleLogoEnter}
-                role="menuitem"
                 ref={el => {
                 logoRef.current = el;
                 }}
